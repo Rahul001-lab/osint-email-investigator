edca6ec1fbda191fd657fd28f9aff74ffe2844c1
Rahul Tewatia <tewatiar236@gmail.com>
2026-08-16 22:06:04 +0530

    Worked on adding new feature

---
+++ Modules/disposable_email.py
@@ -0,0 +1,13 @@
+def disposable_email(email):
+   
+    disposable_domains = [
+        "10minutemail.com",
+        "guerrillamail.com",
+        "mailinator.com",
+        "tempmail.org"
+    ]
+
+    domain = email.split("@")[1]
+    return domain in disposable_domains
+
+ 
